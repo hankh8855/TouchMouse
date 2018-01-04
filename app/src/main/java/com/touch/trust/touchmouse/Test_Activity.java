da049cd808067d9196690b7de7008bdd2b9439d7
han <hankh8855@gmail.com>
2018-01-04 11:02:02 +0900

    no message

---
+++ app/src/main/java/com/touch/trust/touchmouse/Test_Activity.java
@@ -0,0 +1,13 @@
+package com.touch.trust.touchmouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Test_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
